1ee3a379a3f51788f8f6c54dbd58440a2af5b5c0
Eric Livergood <eric.livergood@gmail.com>
2013-11-27 17:43:07 -0500

    static puzzle

--- js/sudoku.js
+++ js/sudoku.js
@@ -11,8 +11,20 @@ var sudoku = function(){
         for(var i = 0; i < 9; i++){
             self.rows.push(new puzzleRow());
             for(var j = 0; j < 9; j++){
-                self.rows()[i].columns.push(j)
+                self.rows()[i].columns.push(staticPuzzle[i][j])
             }
         }
     }
 }
+
+var staticPuzzle = [
+  [6, ,3, , ,5, ,9,4]
+, [7,4, , , , , , , ]
+, [ ,8,9, ,7,1, , ,2]
+, [ ,5, ,3,1, , , , ]
+, [1,3, ,8, ,4, ,2,6]
+, [ , , , ,2,7, ,3, ]
+, [8, , ,7,5, ,4,1, ]
+, [ , , , , , , ,5,9]
+, [3,2, ,1, , ,6, ,8]
+]
